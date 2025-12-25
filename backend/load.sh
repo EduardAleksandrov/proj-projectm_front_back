1fd8790708f102a04d05454c408f1edc9cf655fa
Eduard Aleksandrov <eduard.ale@list.ru>
2025-12-25 20:49:01 +0300

    backend: добавлены методы очистки docker

--- backend/load.sh
+++ backend/load.sh
@@ -54,16 +54,60 @@ function cont() {
     # docker контейнер
     docker exec -it baseservice_service /bin/sh
 }
+
+# Сохранение базы данных на сервере
 function postgre-init-dump() {
     docker exec -it baseservice_postgresql bash
     pg_dump -U user -d db-init -f /var/lib/postgresql/data/db-init_dump.sql
+    pg_dump -U user -d db-init -f /var/lib/postgresql/data/db-init_dump_$(date +%Y%m%d).sql
     exit
     docker cp baseservice_postgresql:/var/lib/postgresql/data/db-init_dump.sql ./db-init_dump.sql
 }
-function clear-docker() {
-    docker system prune
+    # Рабочая функция для сервера
+function postgre-init-dump-server() {
+    # Создаем дамп с датой в имени файла
+    docker exec baseservice_postgresql bash -c "\
+        pg_dump -U user -d db-init -f /var/lib/postgresql/data/db-init_dump_\$(date +%Y%m%d).sql
+    "
+    
+    # Копируем файл дампа на хост
+    docker cp baseservice_postgresql:/var/lib/postgresql/data/db-init_dump_$(date +%Y%m%d).sql ./db-init_dump_$(date +%Y%m%d).sql
 }
 
+# Команды очистки
+function clear-docker() {
+    docker system prune # для всех неиспользуемых объектов (образы, контейнеры, тома). 
+    docker builder prune --all # для кэша 
+
+    docker image prune #(для неиспользуемых образов)
+    docker container prune 
+    docker volume prune
+    docker network prune
+}
+# Для сервера
+function clear-docker() {
+    create-network # Первое, что сделать на сервере. В конвеер развертывания не входит.
+    mkdir files
+
+    postgre-init-dump-server
+    cd backend
+    down
+    cd ../frontend
+    down
+
+    docker container prune
+    docker image prune
+    rm -rf backend
+    rm -rf frontend
+
+    copy frontend
+    cd frontend
+    up
+    copy backend
+    cd backend
+    up
+    Entity_Framework
+}
 case "$1" in
     construct-up)
         construct-up
@@ -97,8 +141,5 @@ case "$1" in
         ;;
     devdown)
         devdown
-        ;;
-    cont)
-        cont
         ;;
 esac
