501bde58c9829b89f2a01d343a7efbdadab95861
Eduard Aleksandrov <eduard.ale@list.ru>
2025-12-25 14:51:40 +0300

    backend: nginx part 1

--- backend/load.sh
+++ backend/load.sh
@@ -16,18 +16,18 @@ function construct-down() {
 
 # Production
 function up() {
-    docker-compose --env-file eprod.env -f docker-compose.yml up --build
+    docker-compose --env-file .env.prod -f docker-compose.yml up --build
 }
 function down() {
-    docker-compose --env-file eprod.env -f docker-compose.yml down
+    docker-compose --env-file .env.prod -f docker-compose.yml down
 }
 
 # Development
 function devup() {
-    docker-compose --env-file edev.env -f docker-compose.yml up --build
+    docker-compose --env-file .env.dev -f docker-compose.yml up --build
 }
 function devdown() {
-    docker-compose --env-file edev.env -f docker-compose.yml down
+    docker-compose --env-file .env.dev -f docker-compose.yml down
 }
 
 function cont() {
